e0320d035a110677bd168c20c908b08c3242d1c1
anneclairebrld <anneclaire.bourland@gmail.com>
2020-01-26 22:15:12 +0100

    web_scrapping several pages / airline companies
    + example data set (not final one)

--- webscraper_skytrax.py
+++ webscraper_skytrax.py
@@ -5,21 +5,18 @@ import requests
 import binascii
 from bs4 import BeautifulSoup 
 
-url_base = 'https://www.airlinequality.com/airline-reviews/air-caraibes/page/1/'
 
-r = requests.get(url_base)
-
-id = 0 
-rows = list()
-
-if r.status_code == 200:
+def get_data(r, airline_name):
+	rows = list()	# this is where the rows of data will be stored
 	soup = BeautifulSoup(r.text, 'html.parser')
 
-	for article in soup.find_all('article', class_ = 'comp'):
+	# grab all review pages
+	for article in soup.find_all('article', class_ = 'comp', itemprop = 'review'):
+		# declare variables for data structure
 		airplane_model = None
 		review_title = None
 		date_of_comment = None
-		verified = None 
+		verified = False 
 		review_title = None 
 		type_of_traveler = None 
 		start_point = None 
@@ -31,35 +28,31 @@ if r.status_code == 200:
 		cabin_staff_service = None
 		food_beverages = None 
 		ground_service = None 
-		value_for_money = None 
-		flight_company = None 
+		value_for_money = None  
 		airplane_model = None
 
+		# check to see if something was posted (if no time then data we are not interested in)
 		if  article.find('time'):
 			if article.find('h2', class_ = 'text_header'):
 				review_title = article.find('h2', class_ = 'text_header').text
 
 			date_of_comment = article.find('time')['datetime'] 
-			overall_rating = article.find('div', class_ = 'rating-10').find('span').text
-			print(review_title)
-			print(date_of_comment)
-			print(overall_rating)
 			review_comment = article.find('div', class_ = 'tc_mobile' ).find('div', class_ = 'text_content')
 			
+			if article.find('div', class_ = 'rating-10').find('span'):
+				overall_rating = article.find('div', class_ = 'rating-10').find('span').text
 
 			if review_comment:
-				verified = False
-				if review_comment.text.split()[0].encode('utf-8') == b'\xe2\x9c\x85':
+				review_comment = review_comment.text
+
+				# check if the comment was verified by someone 
+				if review_comment.split()[0].encode('utf-8') == b'\xe2\x9c\x85':
 					verified = True 
+					review_comment = review_comment.split('|')[1]  # get review comments interesting part
 
-				review_comment = review_comment.text.split('|')[1]
-
+			
+			# grab the stats/basic information of a review
 			for review_stats in article.find_all('table', class_ = 'review-ratings'): #  
-				print('Review_stat found') 
-				
-
-
-				# get all review stats 
 				for tr in review_stats.find_all('tr'):
 					rating_header = tr.find('td', class_ = 'review-rating-header').text.strip().lower() 
 					rating_stars = tr.find('td', class_ = 'review-rating-stars')
@@ -68,7 +61,7 @@ if r.status_code == 200:
 					stat = None
 
 					if rating_stars :
-						stat = rating_stars.find_all('span', class_ = 'star fill')[-1].text
+						stat = len(rating_stars.find_all('span', class_ = 'star fill'))
 					elif rating_text:
 						stat = rating_text.text
 
@@ -94,27 +87,56 @@ if r.status_code == 200:
 					elif rating_header == 'aircraft':
 						airplane_model = stat
 
-					print('%s, %s' % (rating_header, stat )) 
-
-				print('\n')
-
-			
-			review_data = [id, date_of_comment, verified, review_title, review_comment, type_of_traveler, 
+			# create new row of data
+			review_data = [date_of_comment, verified, review_title, review_comment, type_of_traveler, 
 				start_point, end_point, date_flown, overall_rating, 
 				recommended, seat_comfort, cabin_staff_service, food_beverages, 
-				ground_service, value_for_money, flight_company, airplane_model]
+				ground_service, value_for_money, airline_name, airplane_model]
 
+			# add data row to data 
 			rows.append(review_data)
-			id += 1 
+	
+	return rows
 
-columns = ['id', 'date', 'verified', 'review_title', 'review_comment', 'type_of_traveler', 
+
+
+# define colunmns of data
+columns = ['date', 'verified', 'review_title', 'review_comment', 'type_of_traveler', 
 			'start_point', 'end_point', 'date_of_flight', 'overall_rating', 
 			'recommended', 'seat_comfort', 'cabin_staff_service', 'food_beverages', 
 			'ground_service', 'value_for_money', 'flight_company', 'airplane_model']
 
-df = pd.DataFrame(rows, columns = columns)
-print(df.head())
-print(df.tail())
 
-print(df.review_title)
-print(id)
+## define the airline companies 
+airline_companies = ['american-airlines', 'british-airways', 'emirates', 'ryanair', 'virgin-atlantic']
+
+# unique id counter 
+
+
+if __name__ == '__main__':
+	data = list()
+
+	# grab data for each airline
+	for i, airline in enumerate(airline_companies):
+		print('Getting data: %d / %d' % (i, (len(airline_companies)-1 )))
+		# define url_base  
+		url_base = 'https://www.airlinequality.com/airline-reviews/' + airline + '/?sortby=post_date%3ADesc&pagesize=10000'
+
+		try:
+			r = requests.get(url_base)
+		except requests.exceptions.RequestException as e:
+			print('Airline %s ERROR ' % airline)
+			print(e)
+
+		# if no error get the data 
+		if r.status_code == 200:
+			airline_data = get_data(r, airline)
+			r.close()
+
+		data = data + airline_data
+		print('Data for %s loaded' % airline)
+
+	df = pd.DataFrame(data, columns = columns)
+
+	df.to_csv('data.csv', index=False)
+	print('File written')
